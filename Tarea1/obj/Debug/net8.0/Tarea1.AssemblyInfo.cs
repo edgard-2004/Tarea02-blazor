c22db545cb9b46e0b53fff4292cf804b3be58fc3
Edgard Mendez <edgardmendezperez@gmail.com>
2024-09-20 16:34:27 -0400

    Tarea para amadis genao

--- Tarea1/obj/Debug/net8.0/Tarea1.AssemblyInfo.cs
+++ Tarea1/obj/Debug/net8.0/Tarea1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarea1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2c69aa82a40a49a7a8a9c411cf06481d8411d6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarea1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarea1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
